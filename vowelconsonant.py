fd08aee2a9961892a8014758ad83d250c1d2375a
Arulmozhi936 <46783024+Arulmozhi936@users.noreply.github.com>
2019-01-25 13:33:35 +0530

    Update vowelconsonant.py

--- vowelconsonant.py
+++ vowelconsonant.py
@@ -1,7 +1,7 @@
 arul = input()
 if(arul=='A' or arul=='a' or arul=='E' or arul =='e' or arul=='I'
  or arul=='i' or arul=='O' or arul=='o' or arul=='U' or arul=='u'):
-    print("Vowel")
+    print('Vowel')
 else:
-    print("Consonant")
+    print('Consonant')
  

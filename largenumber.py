3415c496c3e67ad5822b3eceb36106f70ee63465
Arulmozhi936 <46783024+Arulmozhi936@users.noreply.github.com>
2019-01-31 20:07:23 +0530

    Update largenumber.py

--- largenumber.py
+++ largenumber.py
@@ -1,12 +1,1 @@
-n1 = int(input())
-n2 = int(input())
-n3 = int(input())
- 
-if (n1 > n2) and (n1 > n3):
-   largest = n1
-elif (n2 > n1) and (n2 > n3):
-   largest = n2
-else:
-   largest = n3
- 
-print(largest)
+print(max(raw_input().split(" ")))

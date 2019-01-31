1b466ca9ca2b94492fd13a0583d71aa25656c9b2
Arulmozhi936 <46783024+Arulmozhi936@users.noreply.github.com>
2019-01-31 21:49:39 +0530

    Create pro1file.py

---
+++ pro1file.py
@@ -0,0 +1,22 @@
+m=int(input())
+x=[]
+b=[]
+c=0
+for i in range(m):
+    s=raw_input()
+    x.append(s)
+for j in range(len(min(x))):
+    for i in range(m):
+        if i<m-1:
+            if x[i][j]==x[i+1][j]:
+               c+=1
+            i-=1
+    if c==m-1:
+         b.append(x[i][j])
+    else:
+         break
+    c=0                    
+if b:
+    print ''.join(b)
+else:
+    print "No matches"

caca71eb5b752317c4315f202ae9eff4c747f658
ashik39 <51356320+ashik39@users.noreply.github.com>
2019-08-05 11:45:40 +0530

    Update python-pro-1.py

--- python-pro-1.py
+++ python-pro-1.py
@@ -1,23 +1,13 @@
 #ashik ali
-m=int(input())
-x=[]
-b=[]
-c=0
-for i in range(m):
-    s=raw_input()
-    x.append(s)
-for j in range(len(min(x))):
-    for i in range(m):
-        if i<m-1:
-            if x[i][j]==x[i+1][j]:
-               c+=1
-            i-=1
-    if c==m-1:
-         b.append(x[i][j])
-    else:
-         break
-    c=0                    
-if b:
-    print ''.join(b)
-else:
-    print "No matches"
+g=int(input())
+df=[]
+for i in range(0,g):
+ ij=input()
+ df.append(ij)
+ff=[]
+for i in zip(*df):
+ if(i.count(i[0])==len(i)):
+  ff.append(i[0])
+ else:
+  break
+print(''.join(ff))
